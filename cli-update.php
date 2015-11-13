4eb5b220415e86bc5a77307a9676d05fb2324cb3
engine <engine@gas-werk.org>
2015-11-13 03:26:29 +0100

    attribute correction during import-migration-phase - work in progress

--- cli-update.php
+++ cli-update.php
@@ -99,7 +99,7 @@ $app->get('/', function () use ($app) {
 	
 	
 	// phase 3: migrate table rawtagdata to table track,album,artist,label
-	$importer->migrateRawtagdataTable();
+	$importer->migrateRawtagdataTable(TRUE);
 	
 	
 	// phase 4: delete dupes of extracted embedded images
