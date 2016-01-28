3f12bc02f9087214347148607e09c9c9dba9536e
engine <engine@gas-werk.org>
2016-01-28 08:43:22 +0100

    fixing play pause state in localplayer and mpdplayer

--- js/app.js
+++ js/app.js
@@ -58,18 +58,20 @@ $(document).ready(function() {
 	/* toggle between mpd-control and local player (jPlayer) */
 	$('.playerModeToggle a').on('click', function(e) {
 		e.preventDefault();
-		if($(this).hasClass('active-mpd') === true) {
+		if(window.sliMpd.currentPlayer.mode === 'mpd') {
 			$(this).addClass('active-local').removeClass('active-mpd').html($(this).attr('data-label-local'));
 			window.sliMpd.currentPlayer = window.sliMpd.localPlayer;
 		} else {
 			$(this).addClass('active-mpd').removeClass('active-local').html($(this).attr('data-label-mpd'));
-			window.sliMpd.currentPlayer = window.sliMpd.mpdPlayer;
 			// pause local player when switching to mpd
-			window.sliMpd.currentPlayer.setPlayPauseState('pause');
+			window.sliMpd.currentPlayer.process({'action':'pause'});
+			window.sliMpd.currentPlayer = window.sliMpd.mpdPlayer;
+			window.sliMpd.currentPlayer.refreshInterval();
+			
 		}
 		$.cookie("playerMode", window.sliMpd.currentPlayer.mode, { expires : 365, path: '/' });
 		$('.player-local,.player-mpd').toggle();
-		window.sliMpd.currentPlayer.drawFavicon();
+		//window.sliMpd.currentPlayer.drawFavicon();
 	});
   
 	//$("#jquery_jplayer_1").jPlayer("play");
