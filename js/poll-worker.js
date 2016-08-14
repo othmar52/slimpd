adc2d4dea1842ea37c25ac97da1c42ef4aeea670
engine <engine@gas-werk.org>
2016-08-14 14:39:02 +0200

    fixed indentations and usage of single/doublequotes

--- js/poll-worker.js
+++ js/poll-worker.js
@@ -1,51 +1,51 @@
 self.pollInterval = 2000;
 self.pollUrl = "/mpdstatus";
-self.addEventListener('message', function(e) {
+self.addEventListener("message", function(e) {
 	var data = e.data;
 	switch (data.cmd) {
-		case 'start':
+		case "start":
 			self.poll();
 			break;
-		case 'setMiliseconds':
+		case "setMiliseconds":
 			if(data.value < 2000) {
-				self.postMessage('Invalid bvalue for miliseconds: ' + data.value + '! terminating worker...');
+				self.postMessage("Invalid bvalue for miliseconds: " + data.value + "! terminating worker...");
 				self.close();
 				break;
 			}
 			self.pollInterval = data.value;
 			break;
-		case 'setPollUrl':
+		case "setPollUrl":
 			self.pollUrl = data.value;
 			break;
-		case 'refreshInterval':
+		case "refreshInterval":
 			clearTimeout(self.poller);
 			self.poll();
 			break;
-		case 'refreshIntervalDelayed':
+		case "refreshIntervalDelayed":
 			clearTimeout(self.poller);
-			setTimeout(function(){self.poll('?force=1');},200);
+			setTimeout(function(){self.poll("?force=1");},200);
 			break;
-		case 'stop':
+		case "stop":
 			self.close();
 			break;
-	};
+	}
 }, false);
 
 self.poll = function(queryString) {
 	var ajax = new XMLHttpRequest();
-	//console.log('pollworker.pollUrl', self.pollUrl);
-	ajax.open("GET", self.pollUrl + ((queryString)?queryString:''), true);
+	//console.log("pollworker.pollUrl", self.pollUrl);
+	ajax.open("GET", self.pollUrl + ((queryString)?queryString:""), true);
 	ajax.onreadystatechange = function(){
-		if(this.readyState == 4){
+		if(this.readyState === XMLHttpRequest.DONE){
 			if(this.status == 200){
 				try {
 					self.postMessage(JSON.parse(this.responseText));
 				} catch(e) {
-					self.postMessage('Poll-response is not parsable as JSON! terminating worker...');
+					self.postMessage("Poll-response is not parsable as JSON! terminating worker...");
 					self.close();
 				}
 			} else{
-				self.postMessage('Response status is not 200 but ' + this.statusText + '! terminating worker...');
+				self.postMessage("Response status is not 200 but " + this.statusText + "! terminating worker...");
 				self.close();
 			}
 		}
@@ -55,5 +55,4 @@ self.poll = function(queryString) {
 		self.poll,
 		self.pollInterval
 	);
-}
-
+};
