3732c9dc19ae25413b25d604d64567bc2986de89
engine <engine@gas-werk.org>
2016-08-14 16:56:01 +0200

    implemented shorthand syntax for JS object methods

--- js/backbone-modules/PageView.js
+++ js/backbone-modules/PageView.js
@@ -2,47 +2,45 @@
  * dependencies: jquery, backbonejs, underscorejs, window.sliMpd.router, window.sliMpd.modules.AbstractView
  */
 (function() {
-    "use strict";
-    
-    var $ = window.jQuery,
-        _ = window._;
-    window.sliMpd = $.extend(true, window.sliMpd, {
-        modules : {}
-    });
-    window.sliMpd.modules.PageView = window.sliMpd.modules.AbstractView.extend({
+	"use strict";
+	
+	var $ = window.jQuery,
+		_ = window._;
+	window.sliMpd = $.extend(true, window.sliMpd, {
+		modules : {}
+	});
+	window.sliMpd.modules.PageView = window.sliMpd.modules.AbstractView.extend({
 
-        name : null,
-        rendered : false,
+		name : null,
+		rendered : false,
 
-        initialize : function(options) {
-            this.name = options.name;
-            this._template = _.template(options.templateString);
-            
-            window.sliMpd.modules.AbstractView.prototype.initialize.call(this, options);
-        },
+		initialize(options) {
+			this.name = options.name;
+			this._template = _.template(options.templateString);
+
+			window.sliMpd.modules.AbstractView.prototype.initialize.call(this, options);
+		},
 
 		/**
 		 * 
  		 * @param bool renderMarkup - if true renders markup using the templateString (set to false on initial page-laod)
 		 */
-        render : function(renderMarkup) {
-        	// only render page once (to prevent multiple click listeners)
-            if (this.rendered) {
-                return;
-            }
-            
-            if (renderMarkup) {
-            	this.el = (this.$el = $(this._template((this.model || {}).attributes)))[0];
-            }
-            
-            window.sliMpd.modules.AbstractView.prototype.render.call(this);
-            this.rendered = true;
-        },
-        
-        remove : function() {
-            window.sliMpd.modules.AbstractView.prototype.remove.call(this);
-        }
-        
-    });
-    
-})();
+		render(renderMarkup) {
+			// only render page once (to prevent multiple click listeners)
+			if (this.rendered) {
+				return;
+			}
+
+			if (renderMarkup) {
+				this.el = (this.$el = $(this._template((this.model || {}).attributes)))[0];
+			}
+
+			window.sliMpd.modules.AbstractView.prototype.render.call(this);
+			this.rendered = true;
+		},
+		
+		remove() {
+			window.sliMpd.modules.AbstractView.prototype.remove.call(this);
+		}
+	});
+}());
