2c7df2fb30f6ef8e2a6821e42e3db2671a67c8ed
engine <engine@gas-werk.org>
2016-08-14 14:08:04 +0200

    fixed indentations and usage of single/doublequotes

--- js/appless.js
+++ js/appless.js
@@ -1,18 +1,15 @@
 $.fn.random = function() {
-  return this.eq(Math.floor(Math.random() * this.length));
-}
+	return this.eq(Math.floor(Math.random() * this.length));
+};
 
 $(document).ready(function() {
-    "use strict";
-    
-    var $ = window.jQuery;
-    window.sliMpd = $.extend(true, window.sliMpd, {
-        modules : {},
-    });
-    
-    window.sliMpd.router = new window.sliMpd.modules.Router();
-    
-    window.Backbone.history.start({
-    	pushState : true
-    });
+	"use strict";
+	var $ = window.jQuery;
+	window.sliMpd = $.extend(true, window.sliMpd, {
+		modules : {},
+	});
+	window.sliMpd.router = new window.sliMpd.modules.Router();
+	window.Backbone.history.start({
+		pushState : true
+	});
 });
