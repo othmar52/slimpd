7920725b89a44adbb1d60a2f23f02970eb317bc7
engine <engine@gas-werk.org>
2016-08-14 15:31:21 +0200

    fixed indentations and usage of single/doublequotes

--- js/backbone-modules/ModalView.js
+++ js/backbone-modules/ModalView.js
@@ -2,38 +2,35 @@
  * dependencies: jquery, backbonejs, underscorejs, window.sliMpd.router, window.sliMpd.modules.AbstractView
  */
 (function() {
-    "use strict";
-    
-    var $ = window.jQuery,
-        _ = window._;
-    window.sliMpd = $.extend(true, window.sliMpd, {
-        modules : {}
-    });
-    window.sliMpd.modules.ModalView = window.sliMpd.modules.AbstractView.extend({
+	"use strict";
+	var $ = window.jQuery,
+		_ = window._;
+	window.sliMpd = $.extend(true, window.sliMpd, {
+		modules : {}
+	});
+	window.sliMpd.modules.ModalView = window.sliMpd.modules.AbstractView.extend({
 
-        rendered : false,
-        $modal : null,
+		rendered : false,
 
-        initialize : function(options) {
-        	//console.log(options);
-        	this.$modal = $('#global-modal');
-            window.sliMpd.modules.AbstractView.prototype.initialize.call(this, options);
-        },
+		$modal : null,
 
-        render : function() {
-            
-            window.sliMpd.modules.AbstractView.prototype.render.call(this);
-            this.rendered = true;
-        },
-        
-        renderModalContent : function(markup) {
-        	$('.modal-content', this.$modal).html(markup);
-        	//this.el = (this.$el = this.$modal.find('.modal-content'))[0];
+		initialize(options) {
+			//console.log(options);
+			this.$modal = $("#global-modal");
+			window.sliMpd.modules.AbstractView.prototype.initialize.call(this, options);
+		},
+
+		render() {
+			window.sliMpd.modules.AbstractView.prototype.render.call(this);
+			this.rendered = true;
+		},
+
+		renderModalContent(markup) {
+			$(".modal-content", this.$modal).html(markup);
 			this.rendered = false;
 			this.render();
-			this.$modal.modal('show');
-        }
-        
-    });
-    
-})();
+			this.$modal.modal("show");
+		}
+	});
+
+}());
