6bde383f5868571c7e314c169e8463afd2d69455
engine <engine@gas-werk.org>
2016-08-14 15:11:51 +0200

    fixed indentations and usage of single/doublequotes

--- js/backbone-modules/Router.js
+++ js/backbone-modules/Router.js
@@ -2,141 +2,124 @@
  * dependencies: jquery, backbonejs, window.sliMpd.modules.PageView
  */
 (function() {
-    "use strict";
-    
-    var $ = window.jQuery;
-    window.sliMpd = $.extend(true, window.sliMpd, {
-        modules : {}
-    });
-    window.sliMpd.modules.Router = window.Backbone.Router.extend({
-    	
-    	rendered : false,
-    	ajaxLoading : false,
+	"use strict";
 
-        currentView : null,
-        previousView : null,
-        $content : null,
-        $body : null,
+	var $ = window.jQuery;
+	window.sliMpd = $.extend(true, window.sliMpd, {
+		modules : {}
+	});
+	window.sliMpd.modules.Router = window.Backbone.Router.extend({
 
-        routes : {
-            '' : 'generic',
-            '*generic' : 'generic',
-            // 'albums/(*generic)' : 'album'
-        },
+		rendered : false,
+		ajaxLoading : false,
 
-        initialize : function(options) {
-            this.$content = $('#main');
-            this.$body = $('body');
-        },
-        
-        navigate : function(fragment, options) {
-        	
-        	
-        	if (this.ajaxLoading) {
-        		return;
-        	}
-        	
-        	/*
-        	 * work against backbone's default behaviour - begin
-        	 *
-        	 * force rendering of view - even if we request the same (current) route again
-        	 * without pushing this to history 
-        	 */
-        	var pathStripper = /#.*$/;
-        	
+		currentView : null,
+		previousView : null,
+		$content : null,
+		$body : null,
+
+		routes : {
+			"" : "generic",
+			"*generic" : "generic",
+			// "albums/(*generic)" : "album"
+		},
+
+		initialize(options) {
+			this.$content = $("#main");
+			this.$body = $("body");
+		},
+
+		navigate(fragment, options) {
+			if (this.ajaxLoading) {
+				return;
+			}
+
+			/*
+			 * work against backbone"s default behaviour - begin
+			 *
+			 * force rendering of view - even if we request the same (current) route again
+			 * without pushing this to history 
+			 */
+			var pathStripper = /#.*$/;
+
 			// Normalize the fragment.
-			fragment = Backbone.history.getFragment(fragment || '');
-			
-			// Don't include a trailing slash on the root.
+			fragment = Backbone.history.getFragment(fragment || "");
+
+			// Don"t include a trailing slash on the root.
 			var rootPath = Backbone.history.root;
-			if (fragment === '' || fragment.charAt(0) === '?') {
-				rootPath = rootPath.slice(0, -1) || '/';
+			if (fragment === "" || fragment.charAt(0) === "?") {
+				rootPath = rootPath.slice(0, -1) || "/";
 			}
 			var url = rootPath + fragment;
-			
+
 			// Strip the hash and decode for matching.
-			fragment = Backbone.history.decodeFragment(fragment.replace(pathStripper, ''));
+			fragment = Backbone.history.decodeFragment(fragment.replace(pathStripper, ""));
 			if (Backbone.history.fragment === fragment) {
 				Backbone.history.loadUrl(fragment);
 				return;
 			}
-			/* work against backbone's default behaviour - end */ 
-			  
-        	window.Backbone.Router.prototype.navigate.call(this, fragment, options);
-        },
+			/* work against backbone"s default behaviour - end */ 
 
-        generic : function(route, queryString) {
-        	var name = ((route === null) ? 'home' : route + '?' + queryString),
-        		url = '/' + ((route === null) ? '' : route + '?' + queryString);
-            
-            // remove view on ajax-done
-            this.previousView = (this.currentView) ? this.currentView : null;
-            
-            // first time page loaded markup is delivered by backend, no need for ajax call!
-            if (!this.rendered) {
-	            this.currentView = new window.sliMpd.modules.PageView({
-	                name : name,
-	                templateString : '',
-	                el : '#main>.main-content'
-	            });
-	            this.currentView.render(false); // renderMarkup flag false, to prevent markup re-rendering
-	            this.rendered = true;
-	            return;
-            }
-            
-            // TODO: add proper loading animation
-            this.$body.addClass('is-loading');
-            NProgress.start();
-			$('<div class="modal-backdrop fade in" id="loading-backdrop"></div>').appendTo(this.$body);
+			window.Backbone.Router.prototype.navigate.call(this, fragment, options);
+		},
+
+		generic(route, queryString) {
+			var name = ((route === null) ? "home" : route + "?" + queryString),
+				url = "/" + ((route === null) ? "" : route + "?" + queryString);
+
+			// remove view on ajax-done
+			this.previousView = (this.currentView) ? this.currentView : null;
+
+			// first time page loaded markup is delivered by backend, no need for ajax call!
+			if (!this.rendered) {
+				this.currentView = new window.sliMpd.modules.PageView({
+					name : name,
+					templateString : "",
+					el : "#main>.main-content"
+				});
+				this.currentView.render(false); // renderMarkup flag false, to prevent markup re-rendering
+				this.rendered = true;
+				return;
+			}
+
+			// TODO: add proper loading animation
+			this.$body.addClass("is-loading");
+			NProgress.start();
+			$("<div class='modal-backdrop fade in' id='loading-backdrop'></div>").appendTo(this.$body);
 			this.ajaxLoading = true;
 			$.ajax({
-				url: window.sliMpd.setGetParameter(url, 'nosurrounding', '1')
+				url: window.sliMpd.setGetParameter(url, "nosurrounding", "1")
 			}).done(function(response) {
 				if(this.previousView) {
 					this.previousView.remove()
 				}
-	            this.currentView = new window.sliMpd.modules.PageView({
-	                name : name,
-	                templateString : response
-	            });
-	            this.currentView.render(true);
-	            this.$content.html(this.currentView.$el);
-	            this.$body.removeClass('is-loading');
-	            NProgress.done();
+				this.currentView = new window.sliMpd.modules.PageView({
+					name : name,
+					templateString : response
+				});
+				this.currentView.render(true);
+				this.$content.html(this.currentView.$el);
+				this.$body.removeClass("is-loading");
+				NProgress.done();
 				$("#loading-backdrop").remove();
 				this.ajaxLoading = false;
 			}.bind(this))
 			.fail(function() {
-				this.$body.removeClass('is-loading');
+				this.$body.removeClass("is-loading");
 				NProgress.done();
 				$("#loading-backdrop").remove();
 				this.ajaxLoading = false;
 				window.sliMpd.notifyError(url);
-    			return;
-  			}.bind(this));
-        },
+				return;
+			}.bind(this));
+		},
 
-        album : function(album) {
-            if (this.currentView && this.currentView.name === 'album/' + album) {
-                return;
-            }
-            if (this.currentView) {
-                this.currentView.remove();
-            }
-
-            this.currentView = new window.sliMpd.modules.AlbumView({
-                name : 'album/' + album,
-                templateString : '' // TODO : get content via ajax
-            });
-            this.currentView.render();
-            this.$content.html(this.currentView.$el);
-        },
 		// FIXME: how to refresh #main view without pushing anything to history?
-		refreshIfName : function(routename) {
+		refreshIfName(routename) {
 			// TODO: check if we can use a generic function
 			// for now lets define each usecase separately
 			switch(routename) {
-				case 'playlist':
+				case "playlist":
 					var urlRegex = new RegExp("^" + window.sliMpd.conf.absRefPrefix.replace("/", "\\/") + "playlist\\/");
 					// TODO: which router variable to use for comparison?
 					// sliMpd.router.$body.context.location.pathname
@@ -153,27 +136,29 @@
 			}
 			return;
 		},
-        
-        // replacing backbones _extractParameters() method
-        //    original method: "%2B" gets replaced with "+"
-        //    overidden method: "%2B" gets preserved
-        // TODO: combinations of "%" and "#" and "\" in urls still does not work
-        // @see: issue #3
-        _extractParameters: function(route, fragment) {
-	      var params = route.exec(fragment).slice(1);
-	      return _.map(params, function(param, i) {
-	        // Don't decode the search params.
-	        if (i === params.length - 1) return param || null;
-	        
-	        return param
-	        	? decodeURIComponent(param)
-	        		.replace(/%/g, '%25')
-	        		.replace(/#/g, '%23')
-	        		.replace(/\+/g, '%2B')
-	        		.replace(/\?/g, '%3F')
-	        	: null;
-	      });
-	    }
-    });
-    
-})();
+
+		// replacing backbones _extractParameters() method
+		//	original method: "%2B" gets replaced with "+"
+		//	overidden method: "%2B" gets preserved
+		// TODO: combinations of "%" and "#" and "\" in urls still does not work
+		// @see: issue #3
+		_extractParameters(route, fragment) {
+			var params = route.exec(fragment).slice(1);
+			return _.map(params, function(param, i) {
+				// Don"t decode the search params.
+				if (i === params.length - 1) {
+					return param;
+				}
+
+				return param
+					? decodeURIComponent(param)
+						.replace(/%/g, "%25")
+						.replace(/#/g, "%23")
+						.replace(/\+/g, "%2B")
+						.replace(/\?/g, "%3F")
+					: null;
+			});
+		}
+	});
+
+}());
