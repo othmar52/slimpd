055ce0e7bb37514a27735d00f68e9074596f858d
engine <engine@gas-werk.org>
2016-08-15 20:01:38 +0200

    renamed short variables

--- php/libs/shims/CompareImages.php
+++ php/libs/shims/CompareImages.php
@@ -4,14 +4,12 @@ namespace Slimpd\Bitmap;
 // from http://compareimages.nikhazy-dizajn.hu/
 class compareImages
 {
-	private function mimeType($i)
-	{
+	private function mimeType($fileName) {
 		/*returns array with mime type and if its jpg or png. Returns false if it isn't jpg or png*/
-		$mime = getimagesize($i);
-		$return = array($mime[0],$mime[1]);
-      
-		switch ($mime['mime'])
-		{
+		$mime = getimagesize($fileName);
+		$return = array($mime[0], $mime[1]);
+
+		switch ($mime['mime']) {
 			case 'image/jpeg':
 				$return[] = 'jpg';
 				return $return;
@@ -21,106 +19,81 @@ class compareImages
 			default:
 				return false;
 		}
-	}  
-    
-	private function createImage($i)
+	}
+
+	private function createImage($fileName)
 	{
 		/*retuns image resource or false if its not jpg or png*/
-		$mime = $this->mimeType($i);
-      
-		if($mime[2] == 'jpg')
-		{
-			return imagecreatefromjpeg ($i);
+		$mime = $this->mimeType($fileName);
+
+		if($mime[2] === 'jpg') {
+			return imagecreatefromjpeg($fileName);
 		} 
-		else if ($mime[2] == 'png') 
-		{
-			return imagecreatefrompng ($i);
+		if ($mime[2] === 'png') {
+			return imagecreatefrompng($fileName);
 		} 
-		else 
-		{
-			return false; 
-		} 
+		return false;
 	}
-    
-	private function resizeImage($i,$source)
-	{
+
+	private function resizeImage($fileName) {
 		/*resizes the image to a 8x8 squere and returns as image resource*/
-		$mime = $this->mimeType($source);
-      
-		$t = imagecreatetruecolor(8, 8);
-		
-		$source = $this->createImage($source);
-		
-		imagecopyresized($t, $source, 0, 0, 0, 0, 8, 8, $mime[0], $mime[1]);
-		
-		return $t;
+		$mime = $this->mimeType($fileName);
+		$img = imagecreatetruecolor(8, 8);
+		$fileName = $this->createImage($fileName);
+		imagecopyresized($img, $fileName, 0, 0, 0, 0, 8, 8, $mime[0], $mime[1]);
+		return $img;
 	}
-    
-    	private function colorMeanValue($i)
-	{
+
+	private function colorMeanValue($i) {
 		/*returns the mean value of the colors and the list of all pixel's colors*/
 		$colorList = array();
 		$colorSum = 0;
-		for($a = 0;$a<8;$a++)
-		{
-		
-			for($b = 0;$b<8;$b++)
-			{
-			
-				$rgb = imagecolorat($i, $a, $b);
+		for($horiz = 0;$horiz<8;$horiz++) {
+			for($verti = 0;$verti<8;$verti++) {
+				$rgb = imagecolorat($i, $horiz, $verti);
 				$colorList[] = $rgb & 0xFF;
 				$colorSum += $rgb & 0xFF;
-				
 			}
-			
 		}
-		
 		return array($colorSum/64,$colorList);
 	}
-    
-    	private function bits($colorMean)
-	{
+
+	private function bits($colorMean){
 		/*returns an array with 1 and zeros. If a color is bigger than the mean value of colors it is 1*/
 		$bits = array();
-		 
-		foreach($colorMean[1] as $color){$bits[]= ($color>=$colorMean[0])?1:0;}
-
+		foreach($colorMean[1] as $color){
+			$bits[]= ($color>=$colorMean[0]) ? 1 : 0;
+		}
 		return $bits;
-
 	}
 	
-    	public function compare($a,$b)
-	{
+	public function compare($fileNameA, $fileNameB) {
 		/*main function. returns the hammering distance of two images' bit value*/
-		$i1 = $this->createImage($a);
-		$i2 = $this->createImage($b);
-		
-		if(!$i1 || !$i2){return false;}
-		
-		$i1 = $this->resizeImage($i1,$a);
-		$i2 = $this->resizeImage($i2,$b);
-		
-		imagefilter($i1, IMG_FILTER_GRAYSCALE);
-		imagefilter($i2, IMG_FILTER_GRAYSCALE);
-		
-		$colorMean1 = $this->colorMeanValue($i1);
-		$colorMean2 = $this->colorMeanValue($i2);
-		
+		$imageA = $this->createImage($fileNameA);
+		$imageB = $this->createImage($fileNameB);
+
+		if(!$imageA || !$imageB){
+			return false;
+		}
+
+		$imageA = $this->resizeImage($imageA, $fileNameA);
+		$imageB = $this->resizeImage($imageB, $fileNameB);
+
+		imagefilter($imageA, IMG_FILTER_GRAYSCALE);
+		imagefilter($imageB, IMG_FILTER_GRAYSCALE);
+
+		$colorMean1 = $this->colorMeanValue($imageA);
+		$colorMean2 = $this->colorMeanValue($imageB);
+
 		$bits1 = $this->bits($colorMean1);
 		$bits2 = $this->bits($colorMean2);
-		
+
 		$hammeringDistance = 0;
-		
-		for($a = 0;$a<64;$a++)
-		{
-		
-			if($bits1[$a] != $bits2[$a])
-			{
+		for($num = 0; $num < 64; $num++) {
+			if($bits1[$num] != $bits2[$num]) {
 				$hammeringDistance++;
 			}
-			
 		}
-		  
 		return $hammeringDistance;
 	}
 }
