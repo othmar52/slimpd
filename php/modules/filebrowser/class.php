14b297fdb1e025ae17a2ec51c039f326fce559e6
engine <engine@gas-werk.org>
2016-08-16 23:51:03 +0200

    cleanup php code

--- php/modules/filebrowser/class.php
+++ php/modules/filebrowser/class.php
@@ -6,153 +6,186 @@ class filebrowser {
 	public $directory;
 	public $base;
 	public $subDirectories = array(
-		'total' => 0,
-		'count' => 0,
-		'dirs' => array()
+		"total" => 0,
+		"count" => 0,
+		"dirs" => array()
 	);
 	public $files = array(
-		'total' => 0,
-		'count' => 0,
-		'music' => array(),
-		'playlist' => array(),
-		'info' => array(),
-		'image' => array(),
-		'other' => array(),
+		"total" => 0,
+		"count" => 0,
+		"music" => array(),
+		"playlist" => array(),
+		"info" => array(),
+		"image" => array(),
+		"other" => array(),
 	);
 	public $breadcrumb = array();
 	
 	public $currentPage = 1;
 	public $itemsPerPage = 20;
-	public $filter = '';
+	public $filter = "";
 	
 	
-	public function getDirectoryContent($d, $ignoreLimit = FALSE, $systemdir=FALSE) {
-		$app = \Slim\Slim::getInstance();
-		if($app->config['mpd']['musicdir'] === '') {
-			$app->flashNow('error', $app->ll->str('error.mpd.conf.musicdir'));
+	public function getDirectoryContent($path, $ignoreLimit = FALSE, $systemdir = FALSE) {
+		$path = $this->checkDirectoryAccess($path, $systemdir);
+
+		if($path === FALSE) {
 			return;
 		}
-		
-		// create helper array only once for performance reasons
-		$extTypes = array();
-		foreach($app->config['musicfiles']['ext'] as $ext) {
-			$extTypes[$ext] = 'music';
-		}
-		foreach($app->config['playlists']['ext'] as $ext) {
-			$extTypes[$ext] = 'playlist';
-		}
-		foreach($app->config['infofiles']['ext'] as $ext) {
-			$extTypes[$ext] = 'info';
-		}
-		foreach($app->config['images']['ext'] as $ext) {
-			$extTypes[$ext] = 'image';
-		}
+		$dir = $path["dir"];
+		$base = $path["base"];
 
+		$this->breadcrumb = self::fetchBreadcrumb($dir);
+		$this->directory = $dir;
 
-		// append trailing slash if missing
-		$d = rtrim($d, DS) . DS;
-		
-		if($systemdir === TRUE) {
-			$base = APP_ROOT;
-			switch($d) {
-				case 'cache/':
-				case 'embedded/':
-				case 'peakfiles/':
-					break;
-				default:
-					die('invalid dir');
-			}
-		} else {
-			$base = $app->config['mpd']['musicdir'];
-			$d = ($d === $base) ? '' : $d;
-
-			if(is_dir($base .$d) === FALSE){ //} || $this->checkAccess($d, $baseDirs) === FALSE) {
-				$app->flashNow('error', $app->ll->str('filebrowser.invaliddir', [$base .$d]));
-				return;
-			}
-
-			if($app->config['filebrowser']['restrict-to-musicdir'] == '1') {
-				// avoid path disclosure outside relevant directories
-				$realpath = realpath($base.$d) . DS;
-
-				if(!$realpath) {
-					$app->flashNow('error', $app->ll->str('filebrowser.realpathempty', [$base.$d]));
-					return;
-				}
-
-				if(stripos($realpath, $app->config['mpd']['musicdir']) !== 0
-				&& stripos($realpath, $app->config['mpd']['alternative_musicdir']) !== 0 ) {
-					$app->flashNow('error', $app->ll->str('filebrowser.outsiderealpath', [$base .$d, $app->config['mpd']['musicdir']]));
-					return;
-				}
-			}
-
-			$this->breadcrumb = self::fetchBreadcrumb($d);
-
-			//if($this->checkAccess($d) === FALSE) {
-			//	die('sorry, you are not allowed to view this directory 8==========D');
-			//}
-
-			// check filesystem permissions
-			if(is_readable($base . $d) === FALSE) {
-				$app->flashNow('error', $app->ll->str('filebrowser.dirpermission', [$d]));
-				return;
-			}
-		}
-
-		$this->directory = $d;
-		
-
-
-		$files = scandir($base . $d);
+		$files = scandir($base . $dir);
 		natcasesort($files);
 		
+		// determine which portion is requestet
 		$minIndex = (($this->currentPage-1) * $this->itemsPerPage);
 		$minIndex = ($minIndex === 0) ? 1 : $minIndex+1;
 		$maxIndex = $minIndex +  $this->itemsPerPage -1;
-		#echo $minIndex . "<br>" . $maxIndex; die();
-		if( count($files) > 2 ) { /* The 2 accounts for . and .. */
-			foreach( $files as $file ) {
-				// skip '.' and '..' and hidden files
-				if(substr($file,0,1) === '.') {
+
+		/* The 2 accounts for . and .. */
+		if( count($files) < 3 ) {
+			return;
+		}
+
+		// create helper array only once for performance reasons
+		$extTypes = $this->getExtMapping();
+		foreach( $files as $file ) {
+			// skip "." and ".." and hidden files
+			if(substr($file,0,1) === ".") {
+				continue;
+			}
+			if( file_exists($base. $dir . $file) === FALSE) {
+				continue;
+			}
+
+			// directories
+			if(is_dir($base . $dir . $file) === TRUE) {
+				$this->subDirectories["total"]++;
+				if($this->filter === "files" && $ignoreLimit === FALSE) {
 					continue;
 				}
-				if( file_exists($base. $d . $file)) {
-					if(is_dir($base . $d . $file) === TRUE) {
-						$this->subDirectories['total']++;
-						if($this->filter === 'files' && $ignoreLimit === FALSE) {
-							continue;
-						}
-						if($this->subDirectories['total'] < $minIndex && $ignoreLimit === FALSE) {
-							continue;
-						}
-						if($this->subDirectories['total'] > $maxIndex && $ignoreLimit === FALSE) {
-							continue;
-						}
-						$this->subDirectories['dirs'][] = new _Directory($d . $file);
-						$this->subDirectories['count']++;
-					} else {
-						$this->files['total']++;
-						if($this->filter === 'dirs' && $ignoreLimit === FALSE) {
-							continue;
-						}
-						if($this->files['total'] < $minIndex && $ignoreLimit === FALSE) {
-							continue;
-						}
-						if($this->files['total'] > $maxIndex && $ignoreLimit === FALSE) {
-							continue;
-						}
-						$f = new File($d . $file);
-						$group = (isset($extTypes[$f->ext]) === TRUE)
-							? $extTypes[$f->ext]
-							: 'other';
-						$this->files[$group][] = new File($d . $file);
-						$this->files['count']++;
-					}
+				if($this->subDirectories["total"] < $minIndex && $ignoreLimit === FALSE) {
+					continue;
 				}
+				if($this->subDirectories["total"] > $maxIndex && $ignoreLimit === FALSE) {
+					continue;
+				}
+				$this->subDirectories["dirs"][] = new _Directory($dir . $file);
+				$this->subDirectories["count"]++;
+				continue;
 			}
+
+			// files
+			$this->files["total"]++;
+			if($this->filter === "dirs" && $ignoreLimit === FALSE) {
+				continue;
+			}
+			if($this->files["total"] < $minIndex && $ignoreLimit === FALSE) {
+				continue;
+			}
+			if($this->files["total"] > $maxIndex && $ignoreLimit === FALSE) {
+				continue;
+			}
+			$f = new File($dir . $file);
+			$group = (isset($extTypes[$f->ext]) === TRUE)
+				? $extTypes[$f->ext]
+				: "other";
+			$this->files[$group][] = new File($dir . $file);
+			$this->files["count"]++;
 		}
-		return ;
+		return;
+	}
+
+	private function getExtMapping() {
+		$app = \Slim\Slim::getInstance();
+		$extTypes = array();
+		foreach($app->config["musicfiles"]["ext"] as $ext) {
+			$extTypes[$ext] = "music";
+		}
+		foreach($app->config["playlists"]["ext"] as $ext) {
+			$extTypes[$ext] = "playlist";
+		}
+		foreach($app->config["infofiles"]["ext"] as $ext) {
+			$extTypes[$ext] = "info";
+		}
+		foreach($app->config["images"]["ext"] as $ext) {
+			$extTypes[$ext] = "image";
+		}
+		return $extTypes;
+	}
+
+	private function checkDirectoryAccess($path, $systemdir) {
+		$app = \Slim\Slim::getInstance();
+		if($app->config["mpd"]["musicdir"] === "") {
+			$app->flashNow("error", $app->ll->str("error.mpd.conf.musicdir"));
+			return FALSE;
+		}
+
+		// append trailing slash if missing
+		$path = rtrim($path, DS) . DS;
+
+		if($systemdir === TRUE) {
+			$base = APP_ROOT;
+			switch($path) {
+				case "cache/":
+				case "embedded/":
+				case "peakfiles/":
+					break;
+				default:
+					$app->flashNow("error", $app->ll->str("filebrowser.invaliddir", [$base .$path]));
+					return FALSE;
+			}
+			return [
+				"base" => APP_ROOT,
+				"dir" => $path
+			];
+		}
+
+		$base = $app->config["mpd"]["musicdir"];
+		$path = ($path === $base) ? "" : $path;
+
+		if(is_dir($base .$path) === FALSE){ //} || $this->checkAccess($path, $baseDirs) === FALSE) {
+			$app->flashNow("error", $app->ll->str("filebrowser.invaliddir", [$base .$path]));
+			return FALSE;
+		}
+
+		// check filesystem permission
+		if(is_readable($base . $path) === FALSE) {
+			$app->flashNow("error", $app->ll->str("filebrowser.dirpermission", [$path]));
+			return FALSE;
+		}
+
+		if($app->config["filebrowser"]["restrict-to-musicdir"] !== "1") {
+			return [
+				"base" => $base,
+				"dir" => $path
+			];
+		}
+
+		// avoid path disclosure outside relevant directories
+		$realpath = realpath($base.$path) . DS;
+
+		if(!$realpath) {
+			$app->flashNow("error", $app->ll->str("filebrowser.realpathempty", [$base.$path]));
+			return FALSE;
+		}
+
+		// and again we do the same musicdir/alternative_musicdir check...
+		// TODO: move this to a proper place
+		if(stripos($realpath, $app->config["mpd"]["musicdir"]) !== 0
+		&& stripos($realpath, $app->config["mpd"]["alternative_musicdir"]) !== 0 ) {
+			$app->flashNow("error", $app->ll->str("filebrowser.outsiderealpath", [$base .$path, $app->config["mpd"]["musicdir"]]));
+			return FALSE;
+		}
+
+		return [
+			"base" => $base,
+			"dir" => $path
+		];
 	}
 
 	/**
@@ -164,29 +197,29 @@ class filebrowser {
 		$app = \Slim\Slim::getInstance();
 		
 		// make sure we have directory separator as last char
-		$d .= (substr($d,-1) !== DS) ? DS : '';
+		$d .= (substr($d,-1) !== DS) ? DS : "";
 		
 		// fetch content of the parent directory
 		$parentDirectory = new \Slimpd\filebrowser();
 		$parentDirectory->getDirectoryContent(dirname($d), TRUE);
-		if($parentDirectory->directory === './') {
+		if($parentDirectory->directory === "./") {
 			$parentDirectory = new \Slimpd\filebrowser();
-			$parentDirectory->getDirectoryContent($app->config['mpd']['musicdir'], TRUE);
+			$parentDirectory->getDirectoryContent($app->config["mpd"]["musicdir"], TRUE);
 		}
 		
 		
 		// iterate over parentdirectories until we find the inputdirectory +1
 		$found = FALSE;
 		
-		foreach($parentDirectory->subDirectories['dirs'] as $subDir) {
+		foreach($parentDirectory->subDirectories["dirs"] as $subDir) {
 			if($found === TRUE) {
 				return $this->getDirectoryContent($subDir->fullpath);
 			}
-			if($subDir->fullpath.'/' == $d) {
+			if($subDir->fullpath."/" == $d) {
 				$found = TRUE;
 			}
 		}
-		$app->flashNow('error', $app->ll->str('filebrowser.nonextdir'));
+		$app->flashNow("error", $app->ll->str("filebrowser.nonextdir"));
 		return $this->getDirectoryContent($d);
 	}
 
@@ -197,27 +230,27 @@ class filebrowser {
 	 */
 	 public function getPreviousDirectoryContent($d) {
 		$app = \Slim\Slim::getInstance();
-		$d .= (substr($d,-1) !== DS) ? DS : '';
+		$d .= (substr($d,-1) !== DS) ? DS : "";
 		$parentDirectory = new \Slimpd\filebrowser();
 		$parentDirectory->getDirectoryContent(dirname($d), TRUE);
-		if($parentDirectory->directory === './') {
+		if($parentDirectory->directory === "./") {
 			$parentDirectory = new \Slimpd\filebrowser();
-			$parentDirectory->getDirectoryContent($app->config['mpd']['musicdir'], TRUE);
+			$parentDirectory->getDirectoryContent($app->config["mpd"]["musicdir"], TRUE);
 		}
 		
 		$prev = 0;
 		
-		foreach($parentDirectory->subDirectories['dirs'] as $subDir) {
-			if($subDir->fullpath.'/' === $d) {
+		foreach($parentDirectory->subDirectories["dirs"] as $subDir) {
+			if($subDir->fullpath."/" === $d) {
 				if($prev === 0) {
-					$app->flashNow('error', $app->ll->str('filebrowser.noprevdir'));
+					$app->flashNow("error", $app->ll->str("filebrowser.noprevdir"));
 					return $this->getDirectoryContent($d);
 				}
 				return $this->getDirectoryContent($prev);
 			}
 			$prev = $subDir->fullpath;
 		}
-		$app->flashNow('error', $app->ll->str('filebrowser.noprevdir'));
+		$app->flashNow("error", $app->ll->str("filebrowser.noprevdir"));
 		return $this->getDirectoryContent($d);
 	}
 
