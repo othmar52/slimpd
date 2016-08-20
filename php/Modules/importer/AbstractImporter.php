03f8f391719b11a068b9f2f9f58cfe2a2e042d9b
engine <engine@gas-werk.org>
2016-08-20 10:50:44 +0200

    simplify importer

---
+++ php/Modules/importer/AbstractImporter.php
@@ -0,0 +1,115 @@
+<?php
+namespace Slimpd\Modules\importer;
+
+abstract class AbstractImporter {
+	protected $jobId;					// mysql record id
+	protected $jobPhase;				// numeric index
+	protected $jobBegin;				// tstamp
+	protected $jobStatusInterval = 5; 	// seconds
+	protected $lastJobStatusUpdate = 0; // timestamp
+
+	// counters needed for calculating estimated time and speed [Tracks/minute]
+	protected $itemCountChecked = 0;	
+	protected $itemCountProcessed = 0;
+	protected $itemCountTotal = 0;
+
+	protected function beginJob($data = array(), $function = '') {
+		cliLog("STARTING import phase " . $this->jobPhase . " " . $function . '()', 1, "cyan");
+		$app = \Slim\Slim::getInstance();
+		$this->jobBegin = microtime(TRUE);
+		$this->itemCountChecked = 0;
+		$this->itemCountProcessed = 0;
+		
+		$relativePath = (isset($data['relativePath']) === TRUE)
+			? $app->db->real_escape_string($data['relativePath'])
+			: '';
+		//$this->itemCountTotal = 0;
+		$query = "INSERT INTO importer
+			(jobPhase, jobStart, jobLastUpdate, jobStatistics, relativePath)
+			VALUES (
+				".(int)$this->jobPhase.",
+				". $this->jobBegin.",
+				". $this->jobBegin. ",
+				'" .serialize($data)."',
+				'". $relativePath ."')";
+		$app->db->query($query);
+		$this->jobId = $app->db->insert_id;
+		$this->lastJobStatusUpdate = $this->jobBegin;
+	}
+
+	public function updateJob($data = array()) {
+		$microtime = microtime(TRUE);
+		if($microtime - $this->lastJobStatusUpdate < $this->jobStatusInterval) {
+			return;
+		}
+		
+		$data['progressPercent'] = 0;
+		$data['microTimestamp'] = $microtime;
+		$this->calculateSpeed($data);
+		
+		$query = "UPDATE importer
+			SET jobStatistics='" .serialize($data)."',
+			jobLastUpdate=".$microtime."
+			WHERE id=" . $this->jobId;
+		\Slim\Slim::getInstance()->db->query($query);
+		cliLog('progress:' . $data['progressPercent'] . '%', 1);
+		$this->lastJobStatusUpdate = $microtime;
+		return;
+	}
+
+	protected function finishJob($data = array(), $function = '') {
+		cliLog("FINISHED import phase " . $this->jobPhase . " " . $function . '()', 1, "cyan");
+		$microtime = microtime(TRUE);
+		$data['progressPercent'] = 100;
+		$data['microTimestamp'] = $microtime;
+		$this->calculateSpeed($data);
+		
+		$query = "UPDATE importer
+			SET jobEnd=".$microtime.",
+			jobLastUpdate=".$microtime.",
+			jobStatistics='" .serialize($data)."' WHERE id=" . $this->jobId;
+		
+		\Slim\Slim::getInstance()->db->query($query);
+		$this->jobId = 0;
+		$this->itemCountChecked = 0;
+		$this->itemCountProcessed = 0;
+		$this->itemCountTotal = 0;
+		$this->lastJobStatusUpdate = $microtime;
+		return;
+	}
+
+	protected function calculateSpeed(&$data) {
+		$data['itemCountChecked'] = $this->itemCountChecked;
+		$data['itemCountProcessed'] = $this->itemCountProcessed;
+		$data['itemCountTotal'] = $this->itemCountTotal;
+
+		// this spped will be relevant for javascript animated progressbar
+		$data['speedPercentPerSecond'] = 0;
+
+		$data['runtimeSeconds'] = $data['microTimestamp'] - $this->jobBegin;
+		if($this->itemCountChecked < 1 || $this->itemCountTotal <1) {
+			return;
+		}
+
+		$seconds = microtime(TRUE) - $this->jobBegin;
+
+		$itemsPerMinute = $this->itemCountChecked/$seconds*60;
+		$data['speedItemsPerMinute'] = floor($itemsPerMinute);
+		$data['speedItemsPerHour'] = floor($itemsPerMinute*60);
+		$data['speedPercentPerSecond'] = ($itemsPerMinute/60)/($this->itemCountTotal/100);
+
+		$minutesRemaining = ($this->itemCountTotal - $this->itemCountChecked) / $itemsPerMinute;
+		if($data['progressPercent'] !== 0) {
+			$data['estimatedRemainingSeconds'] = 0;
+			$data['estimatedTotalRuntime'] = $data['runtimeSeconds'];
+			return;
+		}
+
+		$data['progressPercent'] = floor($this->itemCountChecked / ($this->itemCountTotal/100));
+		// make sure we don not display 100% in case it is not finished
+		$data['progressPercent'] = ($data['progressPercent']>99) ? 99 : $data['progressPercent'];
+		
+		$data['estimatedRemainingSeconds'] = round($minutesRemaining*60);
+		$data['estimatedTotalRuntime'] = round($this->itemCountTotal/$itemsPerMinute*60);
+	}
+}
--- php/Modules/importer/class.php
+++ php/Modules/importer/class.php
@@ -10,19 +10,9 @@ use Slimpd\Models\Genre;
 use Slimpd\Models\Rawtagdata;
 use Slimpd\Models\Bitmap;
 
-class Importer {
+class Importer extends \Slimpd\Modules\importer\AbstractImporter {
 	protected $commonArtworkDirectoryNames = array();
 	protected $directoryImages = array();
-	protected $jobId;					// mysql record id
-	protected $jobPhase;				// numeric index
-	protected $jobBegin;				// tstamp
-	protected $jobStatusInterval = 5; 	// seconds
-	protected $lastJobStatusUpdate = 0; // timestamp
-	
-	// counters needed for calculating estimated time and speed [Tracks/minute]
-	protected $itemCountChecked = 0;	
-	protected $itemCountProcessed = 0;
-	protected $itemCountTotal = 0;
 	
 	// waiting until mpd has finished his internal database-update
 	protected $waitingLoop = 0;
@@ -753,105 +743,6 @@ class Importer {
 		return;
 	}
 
-	private function beginJob($data = array(), $function = '') {
-		cliLog("STARTING import phase " . $this->jobPhase . " " . $function . '()', 1, "cyan");
-		$app = \Slim\Slim::getInstance();
-		$this->jobBegin = microtime(TRUE);
-		$this->itemCountChecked = 0;
-		$this->itemCountProcessed = 0;
-		
-		$relativePath = (isset($data['relativePath']) === TRUE)
-			? $app->db->real_escape_string($data['relativePath'])
-			: '';
-		//$this->itemCountTotal = 0;
-		$query = "INSERT INTO importer
-			(jobPhase, jobStart, jobLastUpdate, jobStatistics, relativePath)
-			VALUES (
-				".(int)$this->jobPhase.",
-				". $this->jobBegin.",
-				". $this->jobBegin. ",
-				'" .serialize($data)."',
-				'". $relativePath ."')";
-		$app->db->query($query);
-		$this->jobId = $app->db->insert_id;
-		$this->lastJobStatusUpdate = $this->jobBegin;
-	}
-	
-	public function updateJob($data = array()) {
-		$microtime = microtime(TRUE);
-		if($microtime - $this->lastJobStatusUpdate < $this->jobStatusInterval) {
-			return;
-		}
-		
-		$data['progressPercent'] = 0;
-		$data['microTimestamp'] = $microtime;
-		$this->calculateSpeed($data);
-		
-		$query = "UPDATE importer
-			SET jobStatistics='" .serialize($data)."',
-			jobLastUpdate=".$microtime."
-			WHERE id=" . $this->jobId;
-		\Slim\Slim::getInstance()->db->query($query);
-		cliLog('progress:' . $data['progressPercent'] . '%', 1);
-		$this->lastJobStatusUpdate = $microtime;
-		return;
-	}
-	
-	private function finishJob($data = array(), $function = '') {
-		cliLog("FINISHED import phase " . $this->jobPhase . " " . $function . '()', 1, "cyan");
-		$microtime = microtime(TRUE);
-		$data['progressPercent'] = 100;
-		$data['microTimestamp'] = $microtime;
-		$this->calculateSpeed($data);
-		
-		$query = "UPDATE importer
-			SET jobEnd=".$microtime.",
-			jobLastUpdate=".$microtime.",
-			jobStatistics='" .serialize($data)."' WHERE id=" . $this->jobId;
-		
-		\Slim\Slim::getInstance()->db->query($query);
-		$this->jobId = 0;
-		$this->itemCountChecked = 0;
-		$this->itemCountProcessed = 0;
-		$this->itemCountTotal = 0;
-		$this->lastJobStatusUpdate = $microtime;
-		return;
-	}
-	
-	private function calculateSpeed(&$data) {
-		$data['itemCountChecked'] = $this->itemCountChecked;
-		$data['itemCountProcessed'] = $this->itemCountProcessed;
-		$data['itemCountTotal'] = $this->itemCountTotal;
-		
-		// this spped will be relevant for javascript animated progressbar
-		$data['speedPercentPerSecond'] = 0;
-		
-		
-		$data['runtimeSeconds'] = $data['microTimestamp'] - $this->jobBegin;
-		if($this->itemCountChecked > 0 && $this->itemCountTotal > 0) {
-			$seconds = microtime(TRUE) - $this->jobBegin;
-			
-			$itemsPerMinute = $this->itemCountChecked/$seconds*60;
-			$data['speedItemsPerMinute'] = floor($itemsPerMinute);
-			$data['speedItemsPerHour'] = floor($itemsPerMinute*60);
-			$data['speedPercentPerSecond'] = ($itemsPerMinute/60)/($this->itemCountTotal/100);
-			
-			$minutesRemaining = ($this->itemCountTotal - $this->itemCountChecked) / $itemsPerMinute;
-			if($data['progressPercent'] === 0) {
-				$data['progressPercent'] = floor($this->itemCountChecked / ($this->itemCountTotal/100));
-				// make sure we don not display 100% in case it is not finished
-				$data['progressPercent'] = ($data['progressPercent']>99) ? 99 : $data['progressPercent'];
-				
-				$data['estimatedRemainingSeconds'] = round($minutesRemaining*60);
-				$data['estimatedTotalRuntime'] = round($this->itemCountTotal/$itemsPerMinute*60);
-			} else {
-				$data['estimatedRemainingSeconds'] = 0;
-				$data['estimatedTotalRuntime'] = $data['runtimeSeconds'];
-			}
-			
-
-		}
-	}
 	
 	// only for development purposes
 	public function tempResetMigrationPhase() {
