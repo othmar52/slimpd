411c65e1d177676caea60c236cf37f89dd632754
engine <engine@gas-werk.org>
2016-08-22 20:45:03 +0200

    simplify php method

--- php/libs/shims/FilesystemUtility.php
+++ php/libs/shims/FilesystemUtility.php
@@ -23,13 +23,11 @@ function rrmdir($dir) {
 	if (is_dir($dir)) {
 		$objects = scandir($dir);
 		foreach ($objects as $object) {
-			if ($object != "." && $object != "..") {
-				if (is_dir($dir."/".$object)) {
-					rrmdir($dir."/".$object);
-				} else {
-					unlink($dir."/".$object);
-				}
+			if ($object === "." || $object === "..") {
+				continue;
 			}
+			$continueWith = (is_dir($dir."/".$object)) ? "rrmdir" : "unlink";
+			$continueWith($dir."/".$object);
 		}
 		rmdir($dir);
 	}
