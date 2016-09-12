c7713aa6a11e06687abeb96e0ab84901b62441ee
engine <engine@gas-werk.org>
2016-09-12 15:01:25 +0200

    added copyright notice to shellscripts

--- scripts/deploy-vendor.sh
+++ scripts/deploy-vendor.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+# Copyright (C) 2016 othmar52 <othmar52@users.noreply.github.com>
+#
+# This file is part of sliMpd - a php based mpd web client
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # TODO: log all invalid paths end echo it after copying
 
 SCRIPT_PATH=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
@@ -38,7 +55,7 @@ find . -type f| while read filepath; do
 		echo "found file $filepath"
 		cp --parents "$filepath" "$SCRIPT_PATH/../$DIR_DEST/"
 	fi
-    #echo "Text read from file: $line"
+	#echo "Text read from file: $line"
 done
 
 # TODO: we need to execute some copied files - pest practice to achieve that?
