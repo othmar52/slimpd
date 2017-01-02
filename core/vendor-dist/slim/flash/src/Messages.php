58399ac03afe4bb41dfbed675a008c722b82658e
engine <engine@gas-werk.org>
2017-01-02 12:37:39 +0100

    updated package slim/flash for having AddMessageNow()

--- core/vendor-dist/slim/flash/src/Messages.php
+++ core/vendor-dist/slim/flash/src/Messages.php
@@ -1,6 +1,10 @@
 <?php
 namespace Slim\Flash;
 
+use ArrayAccess;
+use RuntimeException;
+use InvalidArgumentException;
+
 /**
  * Flash messages
  */
@@ -12,6 +16,13 @@ class Messages
      * @var string[]
      */
     protected $fromPrevious = [];
+
+    /**
+     * Messages for current request
+     *
+     * @var string[]
+     */
+    protected $forNow = [];
 
     /**
      * Messages for next request
@@ -38,19 +49,21 @@ class Messages
      * Create new Flash messages service provider
      *
      * @param null|array|ArrayAccess $storage
+     * @throws RuntimeException if the session cannot be found
+     * @throws InvalidArgumentException if the store is not array-like
      */
     public function __construct(&$storage = null)
     {
         // Set storage
-        if (is_array($storage) || $storage instanceof \ArrayAccess) {
+        if (is_array($storage) || $storage instanceof ArrayAccess) {
             $this->storage = &$storage;
         } elseif (is_null($storage)) {
             if (!isset($_SESSION)) {
-                throw new \RuntimeException('Flash messages middleware failed. Session not found.');
+                throw new RuntimeException('Flash messages middleware failed. Session not found.');
             }
             $this->storage = &$_SESSION;
         } else {
-            throw new \InvalidArgumentException('Flash messages storage must be an array or implement \ArrayAccess');
+            throw new InvalidArgumentException('Flash messages storage must be an array or implement \ArrayAccess');
         }
 
         // Load messages from previous request
@@ -61,20 +74,37 @@ class Messages
     }
 
     /**
-     * Add flash message
+     * Add flash message for the next request
      *
      * @param string $key The key to store the message under
-     * @param string $message Message to show on next request
+     * @param mixed  $message Message to show on next request
      */
     public function addMessage($key, $message)
     {
-        //Create Array for this key
+        // Create Array for this key
         if (!isset($this->storage[$this->storageKey][$key])) {
-            $this->storage[$this->storageKey][$key] = array();
+            $this->storage[$this->storageKey][$key] = [];
         }
-        
-        //Push onto the array
-        $this->storage[$this->storageKey][$key][] = (string)$message;
+
+        // Push onto the array
+        $this->storage[$this->storageKey][$key][] = $message;
+    }
+
+    /**
+     * Add flash message for current request
+     *
+     * @param string $key The key to store the message under
+     * @param mixed  $message Message to show on next request
+     */
+    public function addMessageNow($key, $message)
+    {
+        // Create Array for this key
+        if (!isset($this->forNow[$key])) {
+            $this->forNow[$key] = [];
+        }
+
+        // Push onto the array
+        $this->forNow[$key][] = $message;
     }
 
     /**
@@ -84,18 +114,44 @@ class Messages
      */
     public function getMessages()
     {
-        return $this->fromPrevious;
+        $messages = $this->fromPrevious;
+
+        foreach ($this->forNow as $key => $values) {
+            if (!isset($messages[$key])) {
+                $messages[$key] = [];
+            }
+
+            foreach ($values as $value) {
+                array_push($messages[$key], $value);
+            }
+        }
+
+        return $messages;
     }
-    
+
     /**
      * Get Flash Message
-     * 
+     *
      * @param string $key The key to get the message from
      * @return mixed|null Returns the message
      */
     public function getMessage($key)
     {
-        //If the key exists then return all messages or null
-        return (isset($this->fromPrevious[$key])) ? $this->fromPrevious[$key] : null;
+        $messages = $this->getMessages();
+
+        // If the key exists then return all messages or null
+        return (isset($messages[$key])) ? $messages[$key] : null;
+    }
+
+    /**
+     * Has Flash Message
+     *
+     * @param string $key The key to get the message from
+     * @return bool Whether the message is set or not
+     */
+    public function hasMessage($key)
+    {
+        $messages = $this->getMessages();
+        return isset($messages[$key]);
     }
 }
