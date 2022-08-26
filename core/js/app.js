268ec7c1e424b995ca1ed6cbbc23bf3ca656c528
othmar52 <othmar52@users.noreply.github.com>
2022-08-26 08:38:23 +0200

    [TASK] remove calls to console.log

--- core/js/app.js
+++ core/js/app.js
@@ -264,12 +264,9 @@ $(document).ready(function() {
     });
 
     window.sliMpd.drawFavicon();
-    console.log("rrrrrr", $(".fileModeToggle a").length);
 
     /* toggle between display tags and display filepath */
-    // $(".fileModeToggle a").on("click", function(e) {
     $(document).delegate(".fileModeToggle a", "click", function(e) {
-        console.log("dsxfbgdf");
         e.preventDefault();
         $("body").toggleClass("ffn");
         $(this).find("i").toggleClass("fa-toggle-off").toggleClass("fa-toggle-on");
